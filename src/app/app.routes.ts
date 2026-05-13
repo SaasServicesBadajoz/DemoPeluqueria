654bf666c413dcc4beca217b4fa2d24dc8c7d082
DanielOyola_Casa <danieloyolamoreno@iescastelar.com>
2026-05-13 19:06:26 +0200

    feat: actualizar componentes y añadir páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./pages/inicio/inicio').then(m => m.Inicio)
+  },
+  {
+    path: 'reservas',
+    loadComponent: () => import('./pages/reservas/reservas').then(m => m.Reservas)
+  },
+  {
+    path: 'contacto',
+    loadComponent: () => import('./pages/contacto/contacto').then(m => m.Contacto)
+  }
+];
+
